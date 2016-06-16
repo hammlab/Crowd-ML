82e1c6007ac99d34315a4f13b666fc327979f17e
macchiatoyn <shinyyn@gmail.com>
2016-06-16 00:32:52 -0400

    update iOS client

--- client/iOS/FireBase_Demo/AppDelegate.m
+++ client/iOS/FireBase_Demo/AppDelegate.m
@@ -1,10 +1,23 @@
-//
-//  AppDelegate.m
-//  FireBase_iOS_Client_Demo
-//
-//  Created by JINJIN SHAO and Yani Xie on 3/7/16.
-//  Copyright © 2016 Crowd-ML Team. All rights reserved.
-//
+/**
+ 
+ Copyright 2016 Crowd-ML team
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License
+ 
+ 
+ FireBase_iOS_Client_Demo
+ 
+ **/
 
 #import "AppDelegate.h"
 
---
+++ client/iOS/Pods/Target Support Files/Pods-FireBase_DemoUITests/Pods-FireBase_DemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FireBase_DemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FireBase_DemoUITests
+@end
