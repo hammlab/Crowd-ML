590326788719b585c1934dd45f190bf2a19b44b8
jluken <jacksonluken@gmail.com>
2016-07-07 21:14:23 -0400

    Update Laplace.java

--- client/android/Crowd-ML/app/src/main/java/osu/crowd_ml/Laplace.java
+++ client/android/Crowd-ML/app/src/main/java/osu/crowd_ml/Laplace.java
@@ -2,14 +2,10 @@ package osu.crowd_ml;
 
 /*
 Copyright 2016 Crowd-ML team
-
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
 http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,8 +22,8 @@ public class Laplace implements Distribution{
             sgn = -1;
         else
             sgn = 1;
-        b = noiseScale / sqrt(2)
-        double noise = mu - b*sgn*Math.log(1-2*Math.abs(rng-0.5)); //inverse cumulative laplace Distribution
+        double b = noiseScale / Math.sqrt(2);
+        double noise = mu - noiseScale*sgn*Math.log(1-2*Math.abs(rng-0.5)); //inverse cumulative laplace Distribution
         return noise;
     }
 }
