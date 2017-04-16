e5a07233ee000c81cbabc7ac2ec64b13cb542924
David Soller <soller.23@osu.edu>
2017-04-16 04:06:41 -0400

    Switched to .json config file and annotated server file
    - json file configuration better highlights the parameters when has
    control of
    - rewrote some of the outdated checking material
    - server modifications for better readability
    - checked against the included `configuration.json` file

--- server/crowdML-server.js
+++ server/crowdML-server.js
@@ -1,260 +1,296 @@
 var firebase = require('firebase');
+
+// Server requirements
 var ERROR_CODE = 1;
-var constFile = process.argv[2]
-if (!constFile){
-	console.log("Invalid constants file argument. Server command should be: \'node crowdML-server-Android.js Constants(#).js\'");
-	process.exit(ERROR_CODE);
+var config = {};
+var manage = {};
+
+start()
+
+function start() {
+	loadConfig();
+	validateConfig();
+	manage.test = require('./tests/' + config.testType);
+	setupFirebase();
+	setupListeners();
 }
-var constStr = './'+constFile
-var constants = require(constStr)
 
-firebase.initializeApp({
-		serviceAccount: constants.serviceAccount,
-		databaseURL: constants.databaseURL
-});
-
-var db = firebase.database();
-
-var ref = db.ref();
-var weight = ref.child('trainingWeights');
-var params = ref.child('parameters');
-var users = ref.child('users');
-
-var D = constants.D;
-var maxWeightBatchSize = constants.maxWeightBatchSize;
-var localUpdateNum = constants.localUpdateNum
-var maxGradBatchSize = constants.maxGradBatchSize;
-var c = constants.naughtRate;
-var eps = constants.eps;
-var K = constants.K;
-var descentAlg = constants.descentAlg;
-var testType = constants.testType;
-var testFreq = constants.testFrequency;
-var nh = constants.nh;
-var maxIter = constants.maxIter;
-
-var testNum = 0;
-var changeiter = 0;
-var D = constants.D;
-var weightBatchSize = 0;
-var weightBatch = [];
-var gradBatchSize = 0;
-var gradBatch = [];
-var iter = 1;
-//TODO(tylermzeller) Not sure why this -1 is here. Consider removing.
-var iterArray = [iter, -1];
-var learningRate = c;
-
-var testFile = './'+testType;
-var test = require(testFile);
-
-var auth = firebase.auth();
-var token = auth.createCustomToken("Server");
-
-//reset weights to 0 for testing, initialize learning rate matrices
-var length = D;
-if (K > 2){
-	length = D*K;
-}
-if (constants.lossFunction == 'SoftmaxNN'){
-	length = D*nh + nh + nh*nh + nh + nh*K + K;
-}
-var adaG = new Array(length);
-var rms = new Array(length);
-
-//var trueWeights = require('fs').readFileSync("true-weights.dat").toString()
-//var weightStr = trueWeights.split(", ");
-//function valid(str) {
-//    			return str != "";}
-//		var weightClean = weightStr.filter(valid);
-//		var weightArray = [];
-//		for(var j=0; j<weightClean.length; j++) {
-//			weightArray[j] = parseFloat(weightClean[j], 10);}
-
-
-var initWeight = new Array(length);
-for(i = 0; i < length; i++){
-	initWeight[i] = (Math.random() - 0.5);
-	//initWeight[i] = 1;
-	adaG[i] = 0;
-	rms[i] = 0;
-}
-weightSet = [initWeight, iterArray];
-weight.update({
-	weights: weightSet,
-	iteration: iter
-});
-console.log('[weights initialized        ]');
-
-//send parameters to clients
-params.update({
-	paramIter: constants.paramIter,
-	L: constants.L,
-	noiseScale: constants.noiseScale,
-	noiseDistribution: constants.noiseDistribution,
-	K: K,
-	lossFunction: constants.lossFunction,
-	labelSource: constants.labelSource,
-	featureSource: constants.featureSource,
-	D: D,
-	N: constants.N,
-	clientBatchSize: constants.clientBatchSize,
-	nh: nh,
-	maxIter: maxIter,
-	//necessary for client-side weight calculation
-	localUpdateNum: localUpdateNum,
-	c: c,
-	eps: eps,
-	descentAlg: descentAlg,
-});
-console.log('[parameters set             ]');
-
-var currentWeight;
-var currentIter;
-
-weight.on("value", function(snapshot, prevChildKey) {
-	console.log('[Weights changed            ]');
-	var currentWeights = snapshot.val();
-	weightArrays = currentWeights.weights;
-	currentWeight = weightArrays[0];
-	currentIter = weightArrays[1][0];
-});
-
-
-users.on("child_changed", function(snapshot) {
-	console.log('[User changed               ]');
-	changeiter++;
-	var user = snapshot.val();
-  var grad = user.gradients;
-	var processed = user.gradientProcessed;
-	var userWeightIter = user.weightIter;
-	var userParamIter = user.paramIter;
-	var uid = snapshot.key;
-	var userID = users.child(uid);
-	if(uid && !processed){
-		console.log('WeightIter: ' + userWeightIter + ' ' + iter);
-		if(userWeightIter == iter && userParamIter == constants.paramIter){
-			if(localUpdateNum == 0){
-				addToGradBatch(grad);
-			} else {
-				addToWeightBatch(grad);
-			}
-			console.log('[Updating gradient processed]');
-			userID.update({
-				gradientProcessed: true
-			});
-		}
+function loadConfig() {
+	var cofigFile = process.argv[2];
+	if (!cofigFile) {
+		console.log("Invalid config file argument. Server command should be: \'node crowdML-server-Android.js config(#).js\'");
+		process.exit(ERROR_CODE);
 	}
-});
+	var configFilename = './' + cofigFile;
+	config = require(configFilename);
+	config.file = configFilename;
 
-console.log("[listeners set              ]");
+	// Additions
+	config.c = config.naughtRate;
+	config.testFreq = config.testFrequency;
+	config.weightBatchSize = 0;
+	config.weightBatch = [];
+	config.gradBatchSize = 0;
+	config.gradBatch = [];
+	config.iter = 1;
+	config.testNum = 0;
+	//TODO(tylermzeller) Not sure why this -1 is here. Consider removing.
+	config.iterArray = [config.iter, -1];
 
-function addToGradBatch(gradient){
-	console.log('[Adding to gradient batch   ]');
-	gradBatch.push(gradient);
-	gradBatchSize++;
-	if(gradBatchSize == maxGradBatchSize){
-		var avgGradient = [];
-		for(i = 0; i < gradient.length; i++){
-			var sum = 0;
-			for(j = 0; j < maxGradBatchSize; j++){
-				sum += gradBatch[j][i];
+	var D = config.D;
+	var K = config.K;
+	config.length = D;
+	if (K > 2) {
+		config.length = D * K;
+	}
+	if (config.lossFunction == 'SoftmaxNN') {
+		config.length = D * nh + nh + nh * nh + nh + nh * K + K;
+	}
+	config.adaG = new Array(config.length);
+	config.rms = new Array(config.length);
+	config.initWeight = new Array(config.length);
+	for(i = 0; i < config.length; i++){
+		config.initWeight[i] = (Math.random() - 0.5);
+		//initWeight[i] = 1;
+		config.adaG[i] = 0;
+		config.rms[i] = 0;
+	}
+	config.weightSet = [config.initWeight, config.iterArray];
+}
+
+function setupFirebase() {
+	firebase.initializeApp({
+		serviceAccount: config.serviceAccount,
+		databaseURL: config.databaseURL
+	});
+
+	var ref = firebase.database().ref();
+	manage.weights = ref.child('trainingWeights');
+	manage.params = ref.child('parameters');
+	manage.users = ref.child('users');
+
+	var auth = firebase.auth();
+	manage.serverToken = auth.createCustomToken("Server");
+}
+
+function validateConfig() {
+	// Update the Server README with changes also
+	var supportedDescentAlgs = ["constant", "adagrad", "simple", "sqrt", "rmsProp"];
+	var supportedTestTypes = ["None", "binaryTest", "multiTest", "NNTest"];
+	var supportedNoiseDistributions = ["NoNoise", "Gaussian", "Laplace"];
+	var supportedLossFunctions = ["LogReg", "Hinge", "Softmax", "SoftmaxNN"];
+
+	if (!supportedDescentAlgs.includes(config.descentAlg)) {
+		console.log(new Error(
+			"ERROR: Invalid descentAlg:\n" +
+			"  Expecting one of the supportedDescentAlgs: " + supportedDescentAlgs + "\n" +
+			"  Instead found: " + config.descentAlg));
+		process.exit(ERROR_CODE);
+	}
+
+	if (!supportedTestTypes.includes(config.testType)) {
+		console.log(new Error(
+			"Error: Invalid testType:\n" +
+			"  Expecting one of the supportedTestTypes: " + supportedTestTypes + "\n" +
+			"  Instead found: " + config.testType));
+		process.exit(ERROR_CODE);
+	}
+
+	if (!supportedNoiseDistributions.includes(config.noiseDistribution)) {
+		console.log(new Error(
+			"Error: Invalid noiseDistribution:\n" +
+			"  Expecting one of the supportedNoiseDistributions: " + supportedNoiseDistributions + "\n" +
+			"  Instead found: " + config.noiseDistribution));
+		process.exit(ERROR_CODE);
+	}
+
+	if (!supportedLossFunctions.includes(config.lossFunction)) {
+		console.log(new Error(
+			"Error: Invalid lossFunction:\n" +
+			"  Expecting one of the supportedLossFunctions: " + supportedLossFunctions + "\n" +
+			"  Instead found: " + config.lossFunction));
+		process.exit(ERROR_CODE);
+	}
+}
+
+/*
+ * 1. Initialized Values
+ * 2. Setup Listeners
+ */
+function setupListeners() {
+	// Initialize Values
+	updateWeights(config.iter, [config.initWeight, config.iterArray]);
+	console.log("[ Init: weights initialized   ]");
+
+	// Send parameters to clients
+	manage.params.update({
+		// Model
+		descentAlg: config.descentAlg,
+		lossFunction: config.lossFunction,
+		paramIter: config.paramIter,
+		D: config.D,
+		c: config.c,
+		K: config.K,
+		L: config.L,
+		N: config.N,
+		nh: config.nh,
+		eps: config.eps,
+		maxIter: config.maxIter,
+		clientBatchSize: config.clientBatchSize,
+		// Necessary for client-side weight calculation
+		localUpdateNum: config.localUpdateNum,
+
+		// Privacy
+		noiseDistribution: config.noiseDistribution,
+		noiseScale: config.noiseScale,
+
+		// Data
+		labelSource: config.labelSource,
+		featureSource: config.featureSource,
+	});
+	console.log("[ Init: parameters set        ]");
+	console.log("[ Init: complete              ]");
+
+	// Setup Listeners
+	manage.weights.on("value", function (snapshot, prevChildKey) {
+		console.log("[ Weights changed             ]");
+		var currentWeights = snapshot.val();
+		var weightArrays = currentWeights.weights;
+		config.currentWeight = weightArrays[0];
+		config.currentIter = weightArrays[1][0];
+	});
+	console.log("[ Listeners: weights active   ]");
+
+	manage.users.on("child_changed", function (snapshot) {
+		console.log("[ User changed                ]");
+		var user = snapshot.val();
+		var grad = user.gradients;
+		var processed = user.gradientProcessed;
+		var userWeightIter = user.weightIter;
+		var userParamIter = user.paramIter;
+		var uid = snapshot.key;
+		var userID = manage.users.child(uid);
+		if (uid && !processed) {
+			console.log('WeightIter: ' + userWeightIter + ' ' + config.iter);
+			if (userWeightIter == config.iter && userParamIter == config.paramIter) {
+				if (config.localUpdateNum == 0) {
+					addToGradBatch(grad);
+				} else {
+					addToWeightBatch(grad);
+				}
+				console.log("[ Updating gradient processed ]");
+				userID.update({
+					gradientProcessed: true
+				});
 			}
-			avgGradient[i] = sum/maxGradBatchSize;
 		}
-		gradBatchSize = 0;
-		gradBatch = [];
+	});
+	console.log("[ Listeners: users active     ]");
+	console.log("[ Listeners: complete         ]");
+	console.log("[ setupListeners complete     ]");
+}
+
+function updateWeights(iter, weights) {
+	manage.weights.update({
+		weights: weights,
+		iteration: iter
+	})
+}
+
+function addToGradBatch(gradient) {
+	console.log("[ Adding to gradient batch    ]");
+	config.gradBatch.push(gradient);
+	config.gradBatchSize++;
+	if (config.gradBatchSize == config.maxGradBatchSize) {
+		var avgGradient = [];
+		for (i = 0; i < gradient.length; i++) {
+			var sum = 0;
+			for (j = 0; j < config.maxGradBatchSize; j++) {
+				sum += config.gradBatch[j][i];
+			}
+			avgGradient[i] = sum / config.maxGradBatchSize;
+		}
+		config.gradBatchSize = 0;
+		config.gradBatch = [];
+
+		console.log("[ Performing descent          ]");
 
 		var newWeight = [];
-
-		console.log('[Performing descent         ]');
-		if(descentAlg=='constant'){
-			learningRate = c;
-			for (i = 0; i < length; i++) {
-				newWeight[i] = currentWeight[i] - (learningRate * avgGradient[i]);
-			}
-		}
-		else if(descentAlg=='simple'){
-			learningRate = c/iter;
-			for (i = 0; i < length; i++) {
-				newWeight[i] = currentWeight[i] - (learningRate * avgGradient[i]);
-			}
-		}
-		else if(descentAlg=='sqrt'){
-			learningRate = c/Math.sqrt(iter);
-			for (i = 0; i < length; i++) {
-				newWeight[i] = currentWeight[i] - (learningRate * avgGradient[i]);
-			}
-		}
-		else if(descentAlg=='adagrad'){
-			for (i = 0; i < length; i++) {
-				adaG[i] += gradient[i]*gradient[i];
-				learningRate = c/Math.sqrt(adaG[i]+eps);
-				newWeight[i] = currentWeight[i] - (learningRate * avgGradient[i]);
-			}
-		}
-		else if(descentAlg=='rmsProp'){
-			for (i = 0; i < length; i++) {
-				rms[i] = 0.9*rms[i] + 0.1*gradient[i]*gradient[i];
-				learningRate = c/Math.sqrt(rms[i]+eps);
-				newWeight[i] = currentWeight[i] - (learningRate * avgGradient[i]);
-			}
+		var c = config.c;
+		var learningRate = c;
+		var length = config.length;
+		var eps = config.eps
+		switch (config.descentAlg) {
+			case 'constant':
+				learningRate = c;
+				for (i = 0; i < length; i++) {
+					newWeight[i] = currentWeight[i] - (learningRate * avgGradient[i]);
+				}
+				break;
+			case 'simple':
+				learningRate = c / config.iter;
+				for (i = 0; i < length; i++) {
+					newWeight[i] = config.currentWeight[i] - (learningRate * avgGradient[i]);
+				}
+				break;
+			case 'sqrt':
+				learningRate = c / Math.sqrt(config.iter);
+				for (i = 0; i < length; i++) {
+					newWeight[i] = config.currentWeight[i] - (learningRate * avgGradient[i]);
+				}
+				break;
+			case 'adagrad':
+				for (i = 0; i < length; i++) {
+					config.adaG[i] += gradient[i] * gradient[i];
+					learningRate = c / Math.sqrt(config.adaG[i] + eps);
+					newWeight[i] = config.currentWeight[i] - (learningRate * avgGradient[i]);
+				}
+				break;
+			case 'rmsProp':
+				for (i = 0; i < length; i++) {
+					config.rms[i] = 0.9 * rms[i] + 0.1 * gradient[i] * gradient[i];
+					learningRate = c / Math.sqrt(config.rms[i] + eps);
+					newWeight[i] = config.currentWeight[i] - (learningRate * avgGradient[i]);
+				}
+			default:
+				console.log(new Error("ERROR: Didn't execute a valid descentAlg"));
 		}
 		addToWeightBatch(newWeight);
-
 	}
 }
 
-function addToWeightBatch(weightArray){
-	console.log('[Adding to weight batch     ]');
-	weightBatch.push(weightArray);
-	weightBatchSize++;
-	if(weightBatchSize == maxWeightBatchSize){
+function addToWeightBatch(weightArray) {
+	console.log("[ Adding to weight batch      ]");
+	config.weightBatch.push(weightArray);
+	config.weightBatchSize++;
+	if (config.weightBatchSize == config.maxWeightBatchSize) {
 		var newWeight = [];
-		for(i = 0; i < weightArray.length; i++){
+		for (i = 0; i < weightArray.length; i++) {
 			var sum = 0;
-			for(j = 0; j < maxWeightBatchSize; j++){
-				sum += weightBatch[j][i];
+			for (j = 0; j < config.maxWeightBatchSize; j++) {
+				sum += config.weightBatch[j][i];
 			}
-			newWeight[i] = sum/maxWeightBatchSize;
+			newWeight[i] = sum / config.maxWeightBatchSize;
 		}
 
-		testNum++;
-		if(testNum == testFreq && testType == 'multiTest'){
-			testNum = 0;
-			console.log('Weight iteration ',iter)
-			test.accuracy(newWeight, constStr);
-		}
-		if(testNum == testFreq && testType == 'binaryTest'){
-			testNum = 0;
-			console.log('Weight iteration ',iter)
-			test.accuracy(newWeight, constStr);
-		}
-		if(testNum == testFreq && testType == 'NNTest'){
-			testNum = 0;
-			console.log('Weight iteration ',iter)
-			test.accuracy(newWeight, constStr);
+		config.testNum++;
+		if (config.testNum == config.testFreq) {
+			config.testNum = 0;
+			console.log('Weight iteration ', config.iter);
+			manage.test.accuracy(newWeight, '.' + config.file);
 		}
 
-		if(localUpdateNum > 0){
-			iter += localUpdateNum;
+		if (config.localUpdateNum > 0) {
+			config.iter += config.localUpdateNum;
 		} else {
-			iter++;
+			config.iter++;
 		}
 
-		console.log('[Updating weights in DB     ]');
-		iterArray = [iter, -1];
-		weightSet = [newWeight, iterArray];
-		weight.update({
-			iteration: iter,
-			weights: weightSet, // Combined from line below
-		});
+		console.log("[ Updating weights in DB      ]");
+		config.iterArray = [config.iter, -1];
+		updateWeights(config.iter, [newWeight, config.iterArray]);
 
-		// TODO: Why two separate db queries?
-		// weight.update({
-		// 	weights: weightSet
-		// });
-		weightBatchSize = 0;
-		weightBatch = [];
+		config.weightBatchSize = 0;
+		config.weightBatch = [];
 	}
 }
