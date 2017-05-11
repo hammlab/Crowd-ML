61cb8de1f081c1c15acc8fe082e0de026533f089
David Soller <dsoller@outlook.com>
2017-05-11 15:06:55 -0400

    Added license to DataComputer

--- client/android/Crowd-ML/app/src/main/java/osu/crowd_ml/utils/DataComputer.java
+++ client/android/Crowd-ML/app/src/main/java/osu/crowd_ml/utils/DataComputer.java
@@ -1,5 +1,22 @@
 package osu.crowd_ml.utils;
 
+/*
+    Copyright 2017 Crowd-ML team
+
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License
+*/
+
 import android.content.Context;
 import android.util.Log;
 
@@ -17,7 +34,6 @@ import osu.crowd_ml.loss_functions.LossFunction;
 import osu.crowd_ml.noise_distributions.Distribution;
 
 public class DataComputer {
-
 
     // Bebugging
     private boolean VERBOSE_DEBUG = true;
@@ -48,8 +64,12 @@ public class DataComputer {
     // Context used to get files
     private Context context;
 
+    /**
+     * Create a new DataComputer.
+     *
+     * @param context -- activity context
+     */
     public DataComputer(Context context) {
-        // Get Context
         this.context = context;
     }
 
@@ -320,5 +340,4 @@ public class DataComputer {
         }
         return yBatch;
     }
-
 }
